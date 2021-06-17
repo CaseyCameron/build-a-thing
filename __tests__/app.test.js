45500e406fd6fd3ff65e3e957cf113af7c37f251
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-17 16:38:54 -0700

    init commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,16 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new order and sends a text message', async () => {
+    const res = await request(app)
+  });
+
+});
