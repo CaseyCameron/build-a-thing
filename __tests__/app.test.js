d1cf78f30c9c4b0de0ceb86e7263087c17836570
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-17 17:31:44 -0700

    GET by id RED

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,28 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-//import Order from '../lib/models/Order.js';
+import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a new order and sends a text message', async () => {
+  it.skip('creates a new order and sends a text message', async () => {
     const res = await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
 
     expect(res.body).toEqual({ id: '1', quantity: 10 });
+  });
+
+  it('creates a GET request for an order by id', async () => {
+    const order = await Order.insert({ quantity: 3 });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
   });
 
 });
