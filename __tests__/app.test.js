c3b13b1bf210ba1e24f8b4a31c1fb525583f8a6a
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-17 17:04:49 -0700

    POST with sms verified

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Order from '../lib/models/Order.js';
+//import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -11,6 +11,10 @@ describe('demo routes', () => {
 
   it('creates a new order and sends a text message', async () => {
     const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
+    expect(res.body).toEqual({ id: '1', quantity: 10 });
   });
 
 });
